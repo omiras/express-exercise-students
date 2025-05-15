cf79eb4fdf7cc25eaf3b2a5e91e6b4cc8d107967
omiras <oscar.miras@gmail.com>
2025-05-15 18:16:17 +0200

    done

--- server3b.js
+++ server3b.js
@@ -8,6 +8,13 @@ app.get("/", (req, res) => {
     res.sendFile(indexRoute);
 });
 
+app.get("/patient/:idPatient", (req, res)=> {
+    const idPatient = req.params.idPatient;
+
+    const fileRoute = path.join(__dirname, "server3b-files", idPatient + ".pdf");
+    res.sendFile(fileRoute);
+});
+
 app.listen(3001, ()=>{
     console.log(`Escuchando peticiones en http://localhost:3001`);
 });
